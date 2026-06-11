28fcea2ae3811c2cc2bec2217e60137bfd496a3b
Omar Nada <omar.ihab.12@gmail.com>
2026-06-11 13:29:58 +0200

    Update sidenav.tsx
    Show the right nav links for manager and admin users

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -2,9 +2,10 @@ import Link from 'next/link';
 import NavLinks from '@/app/ui/dashboard/nav-links';
 import VinslonLogo from '@/app/ui/vinslon-logo';
 import { PowerIcon } from '@heroicons/react/24/outline';
-import { signOut } from '@/auth';
+import { signOut, auth } from '@/auth';
 
-export default function SideNav() {
+export default async function SideNav() {
+  const session = await auth();
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
       <Link
@@ -16,12 +17,12 @@ export default function SideNav() {
         </div>
       </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
-        <NavLinks />
+        <NavLinks role={session?.user?.role} />
         <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
         <form
           action={async () => {
             'use server';
-            await signOut({ redirectTo: '/' });
+            await signOut({ redirectTo: '/dashboard' });
           }}
         >
           <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-red-50 hover:text-red-700 md:flex-none md:justify-start md:p-2 md:px-3">
